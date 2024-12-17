77efb92aef7f32131ef2b4acbdaee77df23d98b6
Grigary1 <grigaryvarkey16@gmail.com>
2024-12-17 12:43:38 +0530

    updates

---
+++ app/src/main/java/com/hospital/movieapp/DashboardActivity.java
@@ -0,0 +1,51 @@
+package com.hospital.movieapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DashboardActivity extends AppCompatActivity {
+    Button b1,b2,b3,b4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dashboard);
+        b1=(Button) findViewById(R.id.addMovies);
+        b2=(Button) findViewById(R.id.searchMovies);
+        b3=(Button) findViewById(R.id.viewMovies);
+        b4=(Button) findViewById(R.id.exit);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent ob=new Intent(getApplicationContext(),)
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent ob=new Intent(getApplicationContext(),)
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent ob=new Intent(getApplicationContext(),)
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(ob);
+            }
+        });
+    }
+}
--- app/src/main/java/com/hospital/movieapp/MainActivity.java
+++ app/src/main/java/com/hospital/movieapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.hospital.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +16,31 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     EditText e1,e2;
+    Button b1;
     String getUname , getPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getUname=(EditText) findViewById(R.id.username);
-        getPass=(EditText) findViewById(R.id.password);
+        e1=(EditText) findViewById(R.id.username);
+        e2=(EditText) findViewById(R.id.password);
+        b1=(Button) findViewById(R.id.loginBtn);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUname=e1.getText().toString();
+                getPass=e2.getText().toString();
+                System.out.println(getUname);
+                System.out.println(getPass);
+                if(getUname.equals("theatre")&&getPass.equals("123456")){
+                    Intent ob=new Intent(getApplicationContext(), DashboardActivity.class);
+                    startActivity(ob);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Invalid Credentials",Toast.LENGTH_LONG);
+                }
+            }
+        });
     }
 }
